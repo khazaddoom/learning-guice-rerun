d9066672efefc2600b308651820d2211684f87e9
khazadoom <31121986g@gmail.com>
2020-03-21 21:20:28 +0530

    learning guice

--- src/com/juego/leanring/CheckoutService.java
+++ src/com/juego/leanring/CheckoutService.java
@@ -1,14 +1,15 @@
 package com.juego.leanring;
+
 import com.google.inject.Inject;
+
 public class CheckoutService {
     private final Discountable discount;
-
     @Inject
     public CheckoutService(Discountable discount) {
         this.discount = discount;
     }
 
-    public void checkout(double cartAmount) {
+    public void processOrder(double cartAmount) {
         final double revisedCartAmount = cartAmount += (cartAmount * this.discount.getDiscount());
         System.out.println("Total Order value is: " + revisedCartAmount);
     }
--- src/com/juego/leanring/Main.java
+++ src/com/juego/leanring/Main.java
@@ -1,10 +1,17 @@
 package com.juego.leanring;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Injector guice = Guice.createInjector(new DiscountModule());
 
+        CheckoutService checkout = guice.getInstance(CheckoutService.class);
+
+        checkout.processOrder(100);
 
 
     }
