b9581a9ca39bf76ab20a3819f1a6579ed45223de
khazadoom <31121986g@gmail.com>
2020-03-22 20:02:18 +0530

    learning lambda functions in java

--- src/com/juego/leanring/Main.java
+++ src/com/juego/leanring/Main.java
@@ -5,14 +5,23 @@ import com.google.inject.Injector;
 
 public class Main {
 
+    public interface Email {
+        String constructEmail(String name);
+    }
+
+    public void something(String name, Email email) {
+        System.out.println(email.constructEmail(name));
+    }
+
     public static void main(String[] args) {
 
-        Injector guice = Guice.createInjector(new DiscountModule());
+//        Injector guice = Guice.createInjector(new DiscountModule());
+//
+//        CheckoutService checkout = guice.getInstance(CheckoutService.class);
+//
+//        checkout.processOrder(100);
 
-        CheckoutService checkout = guice.getInstance(CheckoutService.class);
-
-        checkout.processOrder(100);
-
+        new Main().something("ganesh.a", (s) -> s + "@juegostudio.com");
 
     }
 }
